571b3687a904e321ee5a1501797b9dd8e8aee513
Pol Dellaiera <pol.dellaiera@protonmail.com>
2021-11-20 19:14:42 +0100

    Add Kernel tests.

---
+++ tests/Fixtures/App/src/Service/B.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace tests\App\Service;
+
+final class B implements FooInterface
+{
+}
